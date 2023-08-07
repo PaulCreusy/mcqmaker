30c0022a7a3ece78b391c8a32f571943d2fd015f
Paul_Creusy <paul.creusy@gmail.com>
2023-08-07 19:36:01 +0200

    new test for pytest

--- mcq_maker_tools/__init__.py
+++ mcq_maker_tools/__init__.py
@@ -1,3 +1,3 @@
-from mcq_maker_tools.tools_export import *
-from mcq_maker_tools.tools_scrollview import *
-from mcq_maker_tools.tools_enhanced_print import *
+# from mcq_maker_tools.tools_export import *
+# from mcq_maker_tools.tools_scrollview import *
+# from mcq_maker_tools.tools_enhanced_print import *
